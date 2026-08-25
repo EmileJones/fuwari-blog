117d26241e60d7be4180f3b5ec3cf91babcd3260
EmileJones <021027shf@gmail.com>
2026-08-25 10:36:04 +0800

    chore: 删除 navBar 中的github

--- src/config.ts
+++ src/config.ts
@@ -44,11 +44,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/EmileJones", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
+		// {
+		// 	name: "GitHub",
+		// 	url: "https://github.com/EmileJones", // Internal links should not include the base path, as it is automatically added
+		// 	external: true, // Show an external link icon and will open in a new tab
+		// },
 	],
 };
 
@@ -58,27 +58,27 @@ export const profileConfig: ProfileConfig = {
 	bio: "And what must we give in return?",
 	links: [
 		// {
-			// name: "Twitter",
-			// icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			// url: "https://twitter.com",
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it's not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
 		// },
 		// {
-		// name: "Steam",
-		// icon: "fa6-brands:steam",
-		// url: "https://store.steampowered.com",
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
 		// },
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
 			url: "https://github.com/EmileJones",
 		},
-    {
-      name: "Gmail",
-      icon: "fa6-solid:envelope",
-      url: "mailto:021027shf@gmail.com",
-    }
+		{
+			name: "Gmail",
+			icon: "fa6-solid:envelope",
+			url: "mailto:021027shf@gmail.com",
+		},
 	],
 };
 
